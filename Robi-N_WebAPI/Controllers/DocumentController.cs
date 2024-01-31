6ae52e709311cac1d5ba9c07ebfa30dea933ef37
Barış Sakızlı <baris.sakizli@bdh.com.tr>
2024-01-31 08:40:30 +0300

    Updated.

--- Robi-N_WebAPI/Controllers/DocumentController.cs
+++ Robi-N_WebAPI/Controllers/DocumentController.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FastReport.Export.PdfSimple;
+using FastReport.Web;
+using MailEntity;
+using MailEntity.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Robi_N_WebAPI.Model.Response;
+using Robi_N_WebAPI.Utility;
+using RobinCore;
+using System.Data;
 
 namespace Robi_N_WebAPI.Controllers
 {
@@ -11,6 +18,23 @@ namespace Robi_N_WebAPI.Controllers
     public class DocumentController : ControllerBase
     {
 
+        MailService mailService = new MailService();
+
+        private readonly AIServiceDbContext _db;
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _appEnvironment;
+        private readonly ILogger<DocumentController> _logger;
+        private IAppSettings _appConfig;
+
+
+        public DocumentController(AIServiceDbContext db, IWebHostEnvironment appEnvironment, IConfiguration configuration, ILogger<DocumentController> logger, IAppSettings appConfig)
+        {
+            _appEnvironment = appEnvironment;
+            _logger = logger;
+            _db = db;
+            _configuration = configuration;
+            _appConfig = appConfig;
+        }
 
         [HttpGet("getpdfpassword")]
         public async Task<ActionResult> getpdfpassword(DateTime? datetime = null)
@@ -98,5 +122,48 @@ namespace Robi_N_WebAPI.Controllers
             }
 
         }
+
+
+
+        [HttpGet("getsgkpdf")]
+        public async Task<ActionResult> getpdf(DateTime date)
+        {
+            string webRootPath = _appEnvironment.WebRootPath; // Get the path to the wwwroot folder
+            WebReport webReport = new WebReport(); // Create a Web Report Object
+            webReport.Report.Load(webRootPath + "/reports/SgkViziteOnayFormu.frx"); // Load the report into the WebReport object
+            var dataSet = new DataSet(); // Create a data source
+            //dataSet.ReadXml(webRootPath + "/reports/nwind.xml"); // Open the xml database
+            var data = await _db.RBN_SGK_HealthReports.Where(x => x.process == 0 && x.BildirimId != null && x.OnaylamaTarihi == date.Date).ToArrayAsync();
+
+            webReport.Report.RegisterData(data, "Reports"); // Register the data source in the report
+            webReport.Report.GetDataSource("Reports").Enabled = true;
+
+            webReport.Report.Prepare();
+
+            Stream stream = new MemoryStream();
+            webReport.Report.Export(new PDFSimpleExport(), stream);
+            stream.Position = 0;
+
+            List<EmailReports> emailReports = new List<EmailReports>();
+            foreach (var item in data)
+            {
+                EmailReports _mailReport = new EmailReports
+                {
+                    AdSoyad = item.AD + " " + item.SOYAD,
+                    KimlikNumarasi = item.TCKIMLIKNO.ToString(),
+                    MedulaRaporId = item.MEDULARAPORID,
+                    RaporTakipNumarasi = item.RAPORTAKIPNO,
+                    OnayReferansId = item.BildirimId,
+                    RaporBaslamaTarihi = item.ABASTAR,
+                    RaporBirisTarihi = item.RAPORBITTAR
+                };
+                emailReports.Add(_mailReport);
+            }
+
+            var tt = mailService.SGKOnayMailGonder(stream, emailReports);
+
+            return File(stream, "application/pdf", Path.GetFileNameWithoutExtension("SGK-Onay-Bildirgesi") + ".pdf");
+        }
+
     }
 }
--- Robi-N_WebAPI/Controllers/SgkViziteController.cs
+++ Robi-N_WebAPI/Controllers/SgkViziteController.cs
@@ -53,46 +53,7 @@ namespace Robi_N_WebAPI.Controllers
         }
 
 
-        [HttpGet("getsgk")]
-        public async Task<ActionResult> getpdf()
-        {
-            string webRootPath = _appEnvironment.WebRootPath; // Get the path to the wwwroot folder
-            WebReport webReport = new WebReport(); // Create a Web Report Object
-            webReport.Report.Load(webRootPath + "/reports/SgkViziteOnayFormu.frx"); // Load the report into the WebReport object
-            var dataSet = new DataSet(); // Create a data source
-            //dataSet.ReadXml(webRootPath + "/reports/nwind.xml"); // Open the xml database
-            var data = await _db.RBN_SGK_HealthReports.Where(x => x.process == 0 && x.BildirimId != null).ToArrayAsync();
-
-            webReport.Report.RegisterData(data, "Reports"); // Register the data source in the report
-            webReport.Report.GetDataSource("Reports").Enabled = true;
-
-            webReport.Report.Prepare();
-
-            Stream stream = new MemoryStream();
-            webReport.Report.Export(new PDFSimpleExport(), stream);
-            stream.Position = 0;
-
-            List<EmailReports> emailReports = new List<EmailReports>();
-            foreach (var item in data)
-            {
-                EmailReports _mailReport = new EmailReports
-                {
-                    AdSoyad = item.AD + " " + item.SOYAD,
-                    KimlikNumarasi = item.TCKIMLIKNO.ToString(),
-                    MedulaRaporId = item.MEDULARAPORID,
-                    RaporTakipNumarasi = item.RAPORTAKIPNO,
-                    OnayReferansId = item.BildirimId,
-                    RaporBaslamaTarihi = item.ABASTAR,
-                    RaporBirisTarihi = item.RAPORBITTAR
-                };
-                emailReports.Add(_mailReport);
-            }
-
-            var tt = mailService.SGKOnayMailGonder(stream, emailReports);
-
-            return File(stream, "application/pdf", Path.GetFileNameWithoutExtension("SGK-Onay-Bildirgesi") + ".pdf");
-        }
-
+       
 
 
 
